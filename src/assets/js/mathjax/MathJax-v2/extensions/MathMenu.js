00314bae724e03b454239e7f08d18b8ede963c30
StoneyDSP <nathanjhood@googlemail.com>
2023-09-26 22:10:28 +0200

    Moved files to sources folder
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/assets/js/mathjax/MathJax-v2/extensions/MathMenu.js
+++ src/assets/js/mathjax/MathJax-v2/extensions/MathMenu.js
@@ -7,7 +7,7 @@
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -1192,7 +1192,7 @@
           },
           tabIndex: 0
       }, u], ["br"], ["br"], ["a", {
-          href: "http://www.mathjax.org/"
+          href: "https://www.mathjax.org/"
       }, ["www.mathjax.org"]], ["span", {
           className: "MathJax_MenuClose",
           id: "MathJax_AboutClose",
--- src/assets/js/mathjax/MathJax.js
+++ src/assets/js/mathjax/MathJax.js
@@ -7,7 +7,7 @@
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -3411,14 +3411,14 @@ if (document.getElementById && document.childNodes && document.createElement) {
                                   i.id = "mathplayer";
                                   i.classid = "clsid:32F66A20-7614-11D4-BD11-00104BD3F987";
                                   g.appendChild(i);
-                                  document.namespaces.add("m", "http://www.w3.org/1998/Math/MathML");
+                                  document.namespaces.add("m", "https://www.w3.org/1998/Math/MathML");
                                   j.mpNamespace = true;
                                   if (document.readyState && (document.readyState === "loading" || document.readyState === "interactive")) {
                                       document.write('<?import namespace="m" implementation="#MathPlayer">');
                                       j.mpImported = true
                                   }
                               } else {
-                                  document.namespaces.add("mjx_IE_fix", "http://www.w3.org/1999/xlink")
+                                  document.namespaces.add("mjx_IE_fix", "https://www.w3.org/1999/xlink")
                               }
                           } catch (m) {}
                       }
