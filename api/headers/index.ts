fe919d3a346a7e44143aa407526177cfb546ef10
StoneyDSP <nathanjhood@googlemail.com>
2023-10-04 08:41:11 +0200

    Enhancing HTTP control with TS
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- api/headers/index.ts
+++ api/headers/index.ts
@@ -1,30 +1,54 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import * as cors from '../_shared/headers';
+import * as Headers from '../_shared/headers';
+import * as Methods from '../_shared/methods';
+import * as UserAgents from '../_shared/useragents';
 
-export default async function handler(request: VercelRequest, response: VercelResponse)
-{
-  // This is needed if you're planning to invoke your function from a browser.
-  if (request.method === 'OPTIONS') {
-    return response.status(200).json({
-      message: 'ok',
-      headers: { ...cors.headers, 'Content-Type': 'application/json; charset=utf-8' },
-      status: 200,
-    });
-  }
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
 
   try {
     request.body;
   } catch (error) {
     return response.status(400).json({
-      error: error.message,
-      headers: { ...cors.headers, 'Content-Type': 'application/json; charset=utf-8' },
       status: 400,
+      error: error.message,
+      headers: {
+        ...Headers.headers,
+        'Content-Type': 'application/json; charset=utf-8'
+      },
     });
   };
 
+  if (request.method === 'POST') {
+    // Reject POST requests
+    return response.status(401).json({
+      status: 401,
+      message: 'forbidden',
+      headers: {
+        ...Headers.headers,
+        'Content-Type': 'application/json; charset=utf-8' },
+
+    });
+  } else if (request.method === 'OPTIONS') {
+    // This is needed if you're planning to invoke your function from a browser.
+    return response.status(200).json({
+      status: 200,
+      message: 'ok',
+      headers: {
+        ...Headers.headers,
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+    });
+  }
+
   return response.status(200).json({
-    query: request.query,
-    headers: { ...cors.headers, 'Content-Type': 'application/json; charset=utf-8' },
     status: 200,
+    query: request.query,
+    headers: {
+      ...Headers.headers,
+      'Content-Type': 'application/json; charset=utf-8'
+    },
   });
 };
--- api/index.ts
+++ api/index.ts
@@ -1,29 +1,148 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import * as cors from './_shared/headers';
+import * as Headers from './_shared/headers';
+import * as Methods from './_shared/methods';
+import * as UserAgents from './_shared/useragents';
+
+const applicationJsonUTF8 = {
+  "Content-Type": "application/json; charset=utf-8"
+};
 
 export default async function handler(request: VercelRequest, response: VercelResponse)
 {
-  // This is needed if you're planning to invoke your function from a browser.
-  if (request.method === 'OPTIONS') {
-    return response.status(200).json({
-      message: 'ok',
-      headers: { ...cors.headers, 'Content-Type': 'application/json; charset=utf-8' },
-      status: 200,
-    });
-  }
+  // We will use this to filter out suspicious requests, such as
+  // 'HEAD' on the preview branch/environment...
+  const env = process.env.NODE_ENV;
+
+  const ua = UserAgents.userAgent(request.headers.toString['user-agent']);
 
   try {
     request.body;
   } catch (error) {
-    return response.status(400).json({
-      error: error.message,
-      headers: { ...cors.headers, 'Content-Type': 'application/json; charset=utf-8' },
+    // Error out now if the request is a mangled one...
+    return console.log(response.status(400).json({
       status: 400,
-    });
+      error: error.message,
+      headers: {
+        ...Headers.headers,
+        applicationJsonUTF8
+      },
+    }));
   };
 
-  return response.status(200).json({
-    headers: { ...cors.headers, 'Content-Type': 'application/json; charset=utf-8' },
-    status: 200,
-  });
+  switch (request.method) {
+
+    case 'GET':
+    //block of code
+    break;
+
+    case 'HEAD':
+    //block of code
+    if (env !== 'production') {
+      // Forbid snoopy requests
+      response.status(401).json({
+        status: 401,
+        message: 'teapot',
+        headers: {
+          ...Headers.headers,
+          applicationJsonUTF8
+        },
+      });
+    } else {
+      response.status(200).json({
+        status: 200,
+        message: 'ok',
+        headers: {
+          ...Headers.headers,
+          applicationJsonUTF8
+        },
+      });
+    };
+    break;
+
+    case 'OPTIONS':
+    if (ua.isBot) {
+      // Reject botty requests
+      response.status(416).json({
+        status: 416,
+        message: 'teapot',
+        headers: {
+          ...Headers.headers,
+          applicationJsonUTF8
+        },
+      });
+
+    } else {
+      // This is needed if you're planning to invoke your function from a browser.
+      response.status(200).json({
+        status: 200,
+        message: 'ok',
+        headers: {
+          ...Headers.headers,
+          applicationJsonUTF8
+        },
+      });
+    };
+    break;
+
+    case 'POST':
+    // Reject all POST requests
+    response.status(401).json({
+      status: 401,
+      message: 'forbidden',
+      headers: {
+        ...Headers.headers,
+        applicationJsonUTF8
+      },
+    });
+    break;
+
+    case 'PUT':
+    // Reject all PUT requests
+    response.status(401).json({
+      status: 401,
+      message: 'forbidden',
+      headers: {
+        ...Headers.headers,
+        applicationJsonUTF8
+      },
+    });
+    break;
+
+    case 'DELETE':
+    // Reject all DELETE requests
+    response.status(401).json({
+      status: 401,
+      message: 'forbidden',
+      headers: {
+        ...Headers.headers,
+        applicationJsonUTF8
+      },
+    });
+    break;
+
+    case 'PATCH':
+    // Reject all PATCH requests
+    response.status(401).json({
+      status: 401,
+      message: 'forbidden',
+      headers: {
+        ...Headers.headers,
+        applicationJsonUTF8
+      },
+    });
+    break;
+
+    default:
+    //block of code
+    response.status(200).json({
+      status: 200,
+      message: 'default response',
+      headers: {
+        ...Headers.headers,
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+    });
+    };
+
+    return console.log(response);
 };
