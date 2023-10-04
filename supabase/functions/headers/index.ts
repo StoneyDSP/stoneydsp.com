f86823de46e15d83d757424b0a2d562f649e0340
StoneyDSP <nathanjhood@googlemail.com>
2023-10-04 05:06:14 +0200

    Added corsHeaders to import map
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- supabase/functions/headers/index.ts
+++ supabase/functions/headers/index.ts
@@ -1,10 +1,8 @@
 // Follow this setup guide to integrate the Deno language server with your editor:
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
-
 import { serve } from "std/server";
-
-import { corsHeaders } from "../_shared/cors.ts";
+import { corsHeaders } from "cors";
 
 console.log(`Function "browser-with-cors" up and running!`)
 
