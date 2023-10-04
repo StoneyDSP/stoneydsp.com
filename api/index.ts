346cbf4dc1a0396efa3c4792180a0040052ac5dd
StoneyDSP <nathanjhood@googlemail.com>
2023-10-04 02:12:10 +0200

    Added initial supabase files
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import * as cors from './shared/headers';
+import * as cors from './_shared/headers';
 
 export default async function handler(request: VercelRequest, response: VercelResponse)
 {
