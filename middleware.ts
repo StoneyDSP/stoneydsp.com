dba899017f5589cb89630ecd7f0fe0d252ca4191
StoneyDSP <nathanjhood@googlemail.com>
2023-10-07 17:20:10 +0200

    Back to headers and policies for now
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,27 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server';
+import { next } from '@vercel/edge';
+import { NextRequest } from 'next/server';
 
-import type { NextRequest } from 'next/server';
-import type { Database } from './types_db';
+export default function middleware(req: NextRequest) {
 
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient<Database>({ req, res })
-  await supabase.auth.getSession()
-  return res
+  // Extract country. Default to US if not found.
+  const country = (req.geo && req.geo.country) || 'US';
+
+  console.log(`Visitor from ${country}`);
+
+  return next({
+    headers: {
+      'Referrer-Policy': 'origin-when-cross-origin',
+      'X-Frame-Options': 'DENY',
+      'X-Content-Type-Options': 'nosniff',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+      "X-UA-Compatible": "ie-edge",
+      "Cache-Control": "max-age=10, s-maxage=86400, immutable",
+      "CDN-Cache-Control": "max-age=60",
+      "Vercel-CDN-Cache-Control": "max-age=3600",
+      "X-DNS-Prefetch-Control": "on",
+      "X-XSS-Protection": "1; mode=block",
+      "Strict-Transport-Security": "max-age=63072000; includeSubDomains; preload"
+    },
+  });
 }
