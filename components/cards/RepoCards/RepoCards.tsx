9f517822b62e74240c63fee2c3851a70a21ad9c2
StoneyDSP <nathanjhood@googlemail.com>
2023-10-17 00:56:31 +0200

    Moved elements into components
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ components/cards/RepoCards/RepoCards.tsx
@@ -0,0 +1,122 @@
+export const repos = [
+
+  {
+    title: 'Biquads',
+    subtitle:
+      'Simple two-pole equalizer with variable oversampling.',
+    url: 'https://github.com/nathanjhood/Biquads',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  },
+  {
+    title: 'UBento',
+    subtitle:
+      'Minimal, bento-box style Ubuntu-based WSL distro front-end.',
+    url: 'https://github.com/nathanjhood/UBento',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  },
+  {
+    title: 'OrfanidisBiquad',
+    subtitle:
+      'Multi-mode Biquad filter for audio analysis purposes using variable BiLinear transforms, processing precision, and oversampling to achieve high-quality results.',
+    url: 'https://github.com/nathanjhood/OrfanidisBiquad',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  },
+  {
+    title: 'CxxWin',
+    subtitle:
+      'Win32API project implementing a COM application window for Windows platforms in C++ using Direct2D.',
+    url: 'https://github.com/nathanjhood/CxxWin',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  },
+  {
+    title: 'NonLinearFilters',
+    subtitle:
+      'A collection of Non-Linear filters of various topologies and types, for analysis and occasional mixing/production uses.',
+    url: 'https://github.com/nathanjhood/NonLinearFilters',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  },
+  {
+    title: 'cmodule',
+    subtitle:
+      'Multi-platform, multi-architecture C/C++ modules that compile into \'<*>.node\' files, which can be imported as JS modules, for example to run in the NodeJS runtime environment as NPM packages.',
+    url: 'https://github.com/nathanjhood/cmodule',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  },
+  {
+    title: 'AudioPlugin-SVF',
+    subtitle:
+      'Simple Multi-Mode State Variable Filter plugin using TPT, built directly from the StoneyDSP/AudioPlugin template repository and a single juce DSP module.',
+    url: 'https://github.com/nathanjhood/AudioPlugin-SVF',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  },
+  {
+    title: 'MSYS2-Toolchain',
+    subtitle:
+      'A set of gentle 1st-order filter types for delicate emphasis/de-emphasis equalization of audio tracks.',
+      url: 'https://github.com/nathanjhood/MSYS2-Toolchain',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  },
+  {
+    title: 'BiLinearEQ',
+    subtitle:
+      'A set of gentle 1st-order filter types for delicate emphasis/de-emphasis equalization of audio tracks.',
+      url: 'https://github.com/nathanjhood/BiLinearEQ',
+    icon: 'M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528',
+  }
+]
+
+export default async function RepoCards() {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+      {repos.map(({ title, subtitle, url, icon }) => (
+        <a
+          key={title}
+          className="relative flex flex-col group rounded-lg border p-6 hover:border-foreground"
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h3 className="font-bold mb-2  min-h-[40px] lg:min-h-[60px]">
+            {title}
+          </h3>
+          <div className="flex flex-col grow gap-4 justify-between">
+            <p className="text-sm opacity-70">{subtitle}</p>
+            <div className="flex justify-between items-center">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="opacity-80 group-hover:opacity-100"
+              >
+                <path
+                  d={icon}
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="ml-2 h-4 w-4 opacity-0 -translate-x-2 group-hover:translate-x-0 group-hover:opacity-100 transition-all"
+              >
+                <polyline points="9 18 15 12 9 6" />
+              </svg>
+            </div>
+          </div>
+        </a>
+      ))}
+    </div>
+  )
+}
