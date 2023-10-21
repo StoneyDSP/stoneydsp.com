f1b4ef2b2cdc29d6fa73870a9947bcff5d742dde
StoneyDSP <nathanjhood@googlemail.com>
2023-10-22 01:51:13 +0200

    Package fix
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
@@ -15,4 +14,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config
+}
