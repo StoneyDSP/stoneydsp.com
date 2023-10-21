045fe83bdc6cc2bf39615a5a703417a97f18e2f9
StoneyDSP <nathanjhood@googlemail.com>
2023-10-22 01:53:07 +0200

    Switched to Supabase Auth UI
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,7 @@
+import { getSession } from '@/app/supabase-server'
+import { redirect } from 'next/navigation'
 import Messages from './messages'
+import AuthForm from './auth-form'
 import BrandBadge from '@/components/StoneyDSPBadge'
 import Footer from '@/components/Footer'
 // import Link from 'next/link'
@@ -12,36 +15,16 @@ export const metadata: Metadata = {
   title: 'Login',
 }
 
-export default function Login() {
+export default async function Login() {
 
-  // const nonce = headers().get('x-nonce')
+  const session = await getSession()
+
+  if (session) {
+    return redirect('/account')
+  }
 
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
-          {/* <Link
-            href="/"
-            className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-            >
-              <polyline points="15 18 9 12 15 6" />
-            </svg>{' '}
-            Back
-          </Link> */}
-        </div>
-      </nav>
 
       <div className="animate-in flex flex-col gap-8 opacity-0 max-w-4xl px-3 py-16 lg:py-24 text-foreground">
         <div className="flex flex-col items-center mb-4 lg:mb-12">
@@ -51,10 +34,12 @@ export default function Login() {
           <div className="flex gap-8 justify-center items-center text-foreground py-8">
             <p>Systems, Web, Audio & Graphics</p>
           </div>
-          {/* <Logo /> */}
         </div>
 
-        <form
+        <AuthForm />
+        <Messages />
+
+        {/* <form
           className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
           action="/auth/sign-in"
           method="post"
@@ -92,7 +77,7 @@ export default function Login() {
             Sign Up
           </button>
           <Messages />
-        </form>
+        </form> */}
         <Footer />
       </div>
     </div>
