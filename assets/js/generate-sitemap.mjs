05d23e58cb7953934b0b68783db911f484df2127
StoneyDSP <nathanjhood@googlemail.com>
2023-10-23 06:08:44 +0200

    Removed module
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- assets/js/generate-sitemap.mjs
+++ assets/js/generate-sitemap.mjs
@@ -1,6 +1,6 @@
 import { writeFileSync } from 'fs';
 import { globby } from 'globby';
-import prettier from 'prettier';
+// import prettier from 'prettier';
 
 async function generate() {
 //   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
