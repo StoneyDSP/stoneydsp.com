c664f831dd6d84a923f916487d062d9572e1a428
StoneyDSP <nathanjhood@googlemail.com>
2023-10-23 02:18:47 +0200

    Fixed build error
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- app/supabase-provider.tsx
+++ app/supabase-provider.tsx
@@ -1,127 +1,136 @@
 'use client'
 
-import { createContext, useContext, useEffect, useState } from 'react'
-import { Session, SupabaseClient, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import type { Database } from '@/types_db'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import type { SupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
-import { Database } from '@/types_db'
-
-type MaybeSession = Session | null
+import { createContext, useContext, useEffect, useState } from 'react'
 
 type SupabaseContext = {
-  supabase: SupabaseClient<any, string>
-  session: MaybeSession
+  supabase: SupabaseClient<Database>;
 }
 
 const Context = createContext<SupabaseContext | undefined>(undefined)
 
 export default function SupabaseProvider({
-  children,
-  session,
+  children
 }: {
   children: React.ReactNode
-  session: MaybeSession
 }) {
-  const supabase = createClientComponentClient<Database>()
+  const [supabase] = useState(() => createPagesBrowserClient())
   const router = useRouter()
+
+  // useEffect(() => {
+  //   const getSupabase = async () => {
+
+  //     const { data: { subscription } } = await supabase.auth.onAuthStateChange((event) => {
+  //     if (event === 'SIGNED_IN')
+  //       router.refresh()
+  //     })
+
+  //     return () => {
+  //       subscription.unsubscribe()
+  //     }
+  //   }
+
+  //   getSupabase()
 
   useEffect(() => {
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange((_, _session) => {
-      if (_session?.access_token !== session?.access_token) {
+    } = supabase.auth.onAuthStateChange((event) => {
+      if (event === 'SIGNED_IN') {
         router.refresh()
       }
     })
 
-    return () => {
-      subscription.unsubscribe()
-    }
-  }, [router, supabase, session])
+  }, [router, supabase])
 
   return (
-    <Context.Provider value={{ supabase, session }}>
+    <Context.Provider value={{ supabase }}>
       <>{children}</>
     </Context.Provider>
   )
 }
 
-export const useSupabase = <
-  Database = any,
-  SchemaName extends string & keyof Database = 'public' extends keyof Database
-    ? 'public'
-    : string & keyof Database
->() => {
-  let context = useContext(Context)
+export const useSupabase = () => {
+  const context = useContext(Context)
 
   if (context === undefined) {
     throw new Error('useSupabase must be used inside SupabaseProvider')
   }
 
-  return context.supabase as SupabaseClient<Database, SchemaName>
-}
-
-export const useSession = () => {
-  let context = useContext(Context)
-
-  if (context === undefined) {
-    throw new Error('useSession must be used inside SupabaseProvider')
-  }
-
-  return context.session
+  return context
 }
 
 // 'use client'
 
-// import type { Database } from '@/types_db'
-// import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
-// import type { SupabaseClient } from '@supabase/auth-helpers-nextjs'
-// import { useRouter } from 'next/navigation'
 // import { createContext, useContext, useEffect, useState } from 'react'
+// import { Session, SupabaseClient, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+// import { useRouter } from 'next/navigation'
+// import { Database } from '@/types_db'
+
+// type MaybeSession = Session | null
 
 // type SupabaseContext = {
-//   supabase: SupabaseClient<Database>;
+//   supabase: SupabaseClient<any, string>
+//   session: MaybeSession
 // }
 
 // const Context = createContext<SupabaseContext | undefined>(undefined)
 
 // export default function SupabaseProvider({
-//   children
+//   children,
+//   session,
 // }: {
 //   children: React.ReactNode
+//   session: MaybeSession
 // }) {
-//   const [supabase] = useState(() => createPagesBrowserClient())
+//   const supabase = createClientComponentClient<Database>()
 //   const router = useRouter()
 
 //   useEffect(() => {
-//     const getSupabase = async () => {
-
-//       const { data: { subscription } } = await supabase.auth.onAuthStateChange((event) => {
-//       if (event === 'SIGNED_IN')
+//     const {
+//       data: { subscription },
+//     } = supabase.auth.onAuthStateChange((_, _session) => {
+//       if (_session?.access_token !== session?.access_token) {
 //         router.refresh()
-//       })
-
-//       return () => {
-//         subscription.unsubscribe()
 //       }
+//     })
+
+//     return () => {
+//       subscription.unsubscribe()
 //     }
-
-//     getSupabase()
-
-//   }, [router, supabase])
+//   }, [router, supabase, session])
 
 //   return (
-//     <Context.Provider value={{ supabase }}>
+//     <Context.Provider value={{ supabase, session }}>
 //       <>{children}</>
 //     </Context.Provider>
 //   )
 // }
 
-// export const useSupabase = () => {
-//   const context = useContext(Context)
+// export const useSupabase = <
+//   Database = any,
+//   SchemaName extends string & keyof Database = 'public' extends keyof Database
+//     ? 'public'
+//     : string & keyof Database
+// >() => {
+//   let context = useContext(Context)
 
 //   if (context === undefined) {
 //     throw new Error('useSupabase must be used inside SupabaseProvider')
 //   }
 
-//   return context
+//   return context.supabase as SupabaseClient<Database, SchemaName>
+// }
+
+// export const useSession = () => {
+//   let context = useContext(Context)
+
+//   if (context === undefined) {
+//     throw new Error('useSession must be used inside SupabaseProvider')
+//   }
+
+//   return context.session
 // }
