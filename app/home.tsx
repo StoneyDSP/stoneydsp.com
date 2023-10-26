3a1e1ad107d7896cc6ae75143022141904f20ad6
StoneyDSP <nathanjhood@googlemail.com>
2023-10-26 10:36:22 +0200

    Recycling current home page
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ app/home.tsx
@@ -0,0 +1,57 @@
+import { getSession, getUser } from '@/utils/supabase-server'
+import { getURL } from '@/utils/helpers'
+import { redirect } from 'next/navigation'
+
+import HRGradient from "@/components/HRGradient"
+import TextLargeBoldCenter from "@/components/TextLargeBoldCenter"
+import RepoCards from "@/components/cards/RepoCards/RepoCards"
+import ResourceCards from "@/components/cards/ResourceCards/ResourceCards"
+import Footer from "@/components/Footer"
+import Link from "next/link"
+
+export const dynamic = 'force-dynamic'
+
+export default async function Home() {
+
+  const [ session, user, ] = await Promise.all([
+    getSession(),
+    getUser()
+  ])
+
+  // if (!session) {
+  //   return redirect(`${getURL()}login`)
+  // }
+
+  return (
+    <div className="w-full flex flex-col items-center">
+
+      <div className="animate-in flex flex-col gap-14 opacity-0 max-w-4xl px-3 py-16 lg:py-24 text-foreground">
+
+        <div className="flex flex-col gap-8 text-foreground">
+          <HRGradient />
+          <TextLargeBoldCenter>
+            Welcome to my workbench.
+          </TextLargeBoldCenter>
+          <HRGradient />
+          <ResourceCards />
+        </div>
+
+        <div className="flex flex-col gap-8 text-foreground">
+          <HRGradient />
+          <TextLargeBoldCenter>
+            Here are some examples of my work:
+          </TextLargeBoldCenter>
+          <HRGradient />
+          <RepoCards />
+        </div>
+
+        {/* <HRGradient /> */}
+
+        <Footer />
+
+        {/* <HRGradient /> */}
+
+      </div>
+    </div>
+  )
+}
