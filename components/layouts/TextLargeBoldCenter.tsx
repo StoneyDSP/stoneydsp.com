12138573c2e12a292766563c44b3fcd26400bc06
Nathan <nathanjhood@googlemail.com>
2023-10-26 15:59:19 +0200

    Update TextLargeBoldCenter.tsx
    Signed-off-by: Nathan <nathanjhood@googlemail.com>

--- components/layouts/TextLargeBoldCenter.tsx
+++ components/layouts/TextLargeBoldCenter.tsx
@@ -4,7 +4,7 @@ export default async function TextLargeBoldCenter({
   children: React.ReactNode
 }) {
   return (
-    <h2 className="text-lg font-bold text-center">
+    <h2 className="text-lg font-bold text-center text-foreground">
       {children}
     </h2>
   )
