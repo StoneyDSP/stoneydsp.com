db00393af05dc688d87a2dbb1696584c0d902a98
StoneyDSP <nathanjhood@googlemail.com>
2023-10-30 09:17:18 +0100

    More meta stuff
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -16,27 +16,48 @@ const nextConfig = {
     ],
   },
   // basePath: '',
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       headers: [
-  //         {
-  //           key: 'Strict-Transport-Security',
-  //           value: '',
-  //         },
-  //         {
-  //           key: 'X-Robots-Tag',
-  //           value: 'all',
-  //         },
-  //         {
-  //           key: 'X-Frame-Options',
-  //           value: 'DENY',
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: '',
+          },
+          {
+            key: 'X-Robots-Tag',
+            value: 'all',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+      {
+        source: "github-readme-stats-two-lime-18.vercel.app/(.*)",
+        headers: [
+          {
+            key: "Vercel-CDN-Cache-Control",
+            value: "max-age=3600"
+          },
+          {
+            key: "CDN-Cache-Control",
+            value: "max-age=60"
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=10, s-maxage=86400, immutable"
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block"
+          }
+        ]
+      }
+    ]
+  },
   // async redirects() {
   //   return [
   //     {
@@ -49,10 +70,10 @@ const nextConfig = {
   // },
 }
 
-// module.exports = nextConfig
+export default nextConfig
 // next.config.js.
-export default () => {
-	// const plugins = [withMDX, withBundleAnalyzer, withTM(['ui', 'common', 'shared-data'])]
-	// return plugins.reduce((acc, next) => next(acc), nextConfig)
-	return nextConfig
-}
+// export default () => {
+// 	// const plugins = [withMDX, withBundleAnalyzer, withTM(['ui', 'common', 'shared-data'])]
+// 	// return plugins.reduce((acc, next) => next(acc), nextConfig)
+// 	return nextConfig
+// }
