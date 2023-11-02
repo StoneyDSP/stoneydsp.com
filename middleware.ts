6295629b60c689f032e9a3afe8c1518eaae571c4
Nathan <nathanjhood@googlemail.com>
2023-11-02 05:24:50 +0100

    Update middleware.ts
    Signed-off-by: Nathan <nathanjhood@googlemail.com>

--- middleware.ts
+++ middleware.ts
@@ -82,7 +82,7 @@ export const config = {
      * - favicon.ico (favicon file)
      */
     {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      source: '/((?!api|_next/static|_next/image|favicon.ico|images).*)',
       missing: [
         { type: 'header', key: 'next-router-prefetch' },
         { type: 'header', key: 'purpose', value: 'prefetch' },
