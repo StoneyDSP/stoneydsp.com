59645bc0b9905f522a142bff6b58f84f94f1ca80
StoneyDSP <nathanjhood@googlemail.com>
2023-11-03 17:25:46 +0100

    Fixed a broken link
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- components/cards/ResourceCards/ResourceCards.tsx
+++ components/cards/ResourceCards/ResourceCards.tsx
@@ -3,7 +3,7 @@ export const resources = [
     title: 'Read about my most popular projects',
     subtitle:
       'Here you will find my personal research and development of audio plugins.',
-    url: 'https://www.stoneydsp.com/nathanjhood',
+    url: 'https://www.stoneydsp.com/projects',
     icon: 'M7 4V20M17 4V20M3 8H7M17 8H21M3 12H21M3 16H7M17 16H21M4 20H20C20.5523 20 21 19.5523 21 19V5C21 4.44772 20.5523 4 20 4H4C3.44772 4 3 4.44772 3 5V19C3 19.5523 3.44772 20 4 20Z',
   },
   {
