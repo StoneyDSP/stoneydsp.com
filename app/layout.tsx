86d787b6a83ae38c12c0e3f75c29d1ba9351f9b3
StoneyDSP <nathanjhood@googlemail.com>
2023-11-06 00:18:53 +0100

    Trying to fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Header, Footer, Main, ConsentBanner } from '@/components/elements'
-import { Analytics } from '@vercel/analytics/server'
+import Analytics from '@vercel/analytics'
 // import Favicon from '@/app/favicon.ico'
 import { Metadata } from 'next'
 import { Suspense } from 'react'
