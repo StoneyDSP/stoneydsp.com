2292f7a33e356ff921e69d8edaff30246f3a3f24
StoneyDSP <nathanjhood@googlemail.com>
2023-11-12 17:35:11 +0100

    Added tabindex for markdown h1-h6
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,9 +4,24 @@ import '@/app/globals.css'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: (({ children }) => <h1>
+    h1: (({ children }) => <h1 tabIndex={-1}>
       {children}
     </h1>),
+    h2: (({ children }) => <h2 tabIndex={-1}>
+      {children}
+    </h2>),
+    h3: (({ children }) => <h3 tabIndex={-1}>
+      {children}
+    </h3>),
+    h4: (({ children }) => <h4 tabIndex={-1}>
+      {children}
+    </h4>),
+    h5: (({ children }) => <h5 tabIndex={-1}>
+      {children}
+    </h5>),
+    h6: (({ children }) => <h6 tabIndex={-1}>
+      {children}
+    </h6>),
     ...components,
   }
 }
