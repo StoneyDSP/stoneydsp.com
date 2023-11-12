c69577beee883bb99beb2ae47d4fc9c48bbf1948
StoneyDSP <nathanjhood@googlemail.com>
2023-11-12 20:18:34 +0100

    Div for centering...
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ components/cards/GitStats/index.tsx
@@ -0,0 +1,34 @@
+export async function GitStatsCardBG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="467"
+      height="195"
+      fill="none"
+      viewBox="0 0 467 195"
+    >
+      <rect
+        width="467"
+        height="195"
+        fill="none"
+        rx="60"
+      />
+    </svg>
+  )
+}
+
+export default async function GitStatsCard() {
+  return (
+    <picture>
+      <source
+        srcSet="https://gh-readme-stats.stoneydsp.com/api?username=nathanjhood&show_icons=true&border_radius=0&hide_border=true&bg_color=00000000&theme=dark"
+        media="(prefers-color-scheme: dark)"
+      />
+      <source
+        srcSet="https://gh-readme-stats.stoneydsp.com/api?username=nathanjhood&show_icons=true&border_radius=0&hide_border=true&bg_color=00000000&theme=default"
+        media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
+      />
+      <img src="https://gh-readme-stats.stoneydsp.com/api?username=nathanjhood&show_icons=true&border_radius=0&hide_border=true&bg_color=00000000" alt="StoneyDSP's GitHub stats" />
+    </picture>
+  )
+}
