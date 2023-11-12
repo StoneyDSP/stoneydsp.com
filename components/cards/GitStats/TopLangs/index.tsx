f6d5fa30ffa3f38f38ea9f8016f829e9aa594953
StoneyDSP <nathanjhood@googlemail.com>
2023-11-12 20:43:01 +0100

    Git stats cards control
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- components/cards/GitStats/TopLangs/index.tsx
+++ components/cards/GitStats/TopLangs/index.tsx
@@ -28,7 +28,28 @@ export default async function GitStatsTopLangsCard() {
         srcSet="https://gh-readme-stats.stoneydsp.com/api/top-langs/?username=nathanjhood&langs_count=8&show_icons=true&hide=tex,css,html&border_radius=0&hide_border=true&bg_color=00000000&theme=default"
         media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
       />
-      <img src="https://gh-readme-stats.stoneydsp.com/api/top-langs/?username=nathanjhood&langs_count=8&show_icons=true&hide=tex,css,html&border_radius=0&hide_border=true&bg_color=00000000" alt="StoneyDSP's Top Languages" />
+      <img
+        src="https://gh-readme-stats.stoneydsp.com/api/top-langs/?username=nathanjhood&langs_count=8&show_icons=true&hide=tex,css,html&border_radius=0&hide_border=true&bg_color=00000000"
+        alt="StoneyDSP's Top Languages"
+        onBlur={
+          () => {
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="300"
+              height="405"
+              fill="none"
+              viewBox="0 0 300 405"
+            >
+              <rect
+                width="300"
+                height="405"
+                fill="none"
+                rx="60"
+              />
+            </svg>
+          }
+        }
+      />
     </picture>
   )
 }
--- components/cards/index.tsx
+++ components/cards/index.tsx
@@ -1,9 +1,11 @@
 import GitStatsCard from '@/components/cards/GitStats'
+import GitStatsTopLangsCard from '@/components/cards/GitStats/TopLangs'
 import RepoCards from '@/components/cards/RepoCards'
 import ResourceCards from '@/components/cards/ResourceCards'
 
 export {
   GitStatsCard,
+  GitStatsTopLangsCard,
   RepoCards,
   ResourceCards
 }
