9cdfb6a66ddbe01296c269a47e7a36978d370180
StoneyDSP <nathanjhood@googlemail.com>
2023-11-12 20:00:06 +0100

    Testing transparent BG
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- components/articles/about/index.tsx
+++ components/articles/about/index.tsx
@@ -53,11 +53,11 @@ export default async function AboutContent() {
           <picture>
             <source
               srcSet="https://gh-readme-stats.stoneydsp.com/api/top-langs/?username=nathanjhood&langs_count=8&show_icons=true&hide=tex,css,html&border_radius=0&hide_border=true&bg_color=00000000&theme=dark"
-              media="(prefers-color-scheme: dark), (prefers-color-scheme: no-preference)"
+              media="(prefers-color-scheme: dark)"
             />
             <source
               srcSet="https://gh-readme-stats.stoneydsp.com/api/top-langs/?username=nathanjhood&langs_count=8&show_icons=true&hide=tex,css,html&border_radius=0&hide_border=true&bg_color=00000000&theme=default"
-              media="(prefers-color-scheme: light)"
+              media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
             />
             <img src="https://gh-readme-stats.stoneydsp.com/api/top-langs/?username=nathanjhood&langs_count=8&show_icons=true&hide=tex,css,html&border_radius=0&hide_border=true&bg_color=00000000" alt="StoneyDSP's Top Languages" />
           </picture>
