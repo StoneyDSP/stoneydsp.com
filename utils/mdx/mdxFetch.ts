70f4c38d48330b38119b81a5fa502acb622e7302
StoneyDSP <nathanjhood@googlemail.com>
2023-11-13 03:26:43 +0100

    Fixup
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- utils/mdx/mdxFetch.ts
+++ utils/mdx/mdxFetch.ts
@@ -1,23 +1,15 @@
 'use server'
-import { MDXRemote } from 'next-mdx-remote/rsc'
 
 export default async function mdxFetch(url: string) {
   try {
     // MDX text - can be from a local file, database, CMS, fetch, anywhere...
     const res = await fetch(url)
     const markdown = await res.text()
-    try {
-      const mdxParsed = await MDXRemote({source: markdown})
-      return mdxParsed.props
-    } catch (error) {
-      console.log(`Failed to parse content ${url}:`, error)
-      const mdxParseFailed = `# 404: Failed to parse due to ${error}`
-      return mdxParseFailed
-    }
+    return markdown
   } catch (error) {
     // TypeError: Failed to fetch
     console.log(`Failed to fetch content ${url}:`, error)
-    const markdownFetchFailed = `# 404: not found due to ${error}`
-    return markdownFetchFailed
+    const markdown = `# 404: not found due to ${error}`
+    return markdown
   }
 }
