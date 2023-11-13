0ca13ac5c3876bf0b8aebd2941a9886b539df414
StoneyDSP <nathanjhood@googlemail.com>
2023-11-13 03:07:23 +0100

    MDX Fetch tests
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- components/articles/projects/msys2toolchain/index.tsx
+++ components/articles/projects/msys2toolchain/index.tsx
@@ -17,7 +17,42 @@ export default async function MsystoolchainContent() {
 
   const mdx = await mdxFetch('https://raw.githubusercontent.com/nathanjhood/MSYS2-Toolchain/master/README.md')
 
-  const mdxParsed = await MDXRemote({source: mdx})
+  // const mdxParsed = await MDXRemote({source: mdx})
+
+//   {`# MSYS2 toolchain
+
+// THIS PAGE FAILED TO RENDER...
+
+// <i>The below is quoted from <a href="https://www.msys2.org/license/">https://www.msys2.org/license/</a></i>
+
+// "MSYS2 is a software distribution consisting of several independent parts, each with their own licenses, comparable to a Linux distribution.
+
+// The installer, for example, is based on the qt-installer-framework and pre-packs the direct and indirect dependencies of the base meta package. Each package has its own licenses.
+
+// The "pacman" package manager in MSYS2 allows users to install other packages available in our repository, each with their own licenses.
+
+// The license information for each package as visible on <a href="https://www.msys2.org/license/">https://packages.msys2.org</a> is maintained on a best effort basis and "we" (quote) make no guarantee that it is accurate or complete."
+
+// ## vcpkg
+
+// vcpkg - C++ Library Manager for Windows, Linux, and MacOS
+
+// Copyright (c) Microsoft Corporation
+
+// vcpkg is distributed under the MIT License
+
+// All rights reserved.
+
+// ## CMake
+
+// CMake - Cross Platform Makefile Generator
+
+// Copyright 2000-2023 Kitware, Inc. and Contributors
+
+// CMake is distributed under the OSI-approved BSD 3-clause License
+
+// All rights reserved.`}
+
 
   return (
     <BlogArticle>
@@ -38,41 +73,7 @@ export default async function MsystoolchainContent() {
               />
             </div>
 
-            <MDXRemote source={mdxParsed ? mdx : `
-# MSYS2 toolchain
-
-THIS PAGE FAILED TO RENDER...
-
-<i>The below is quoted from <a href="https://www.msys2.org/license/">https://www.msys2.org/license/</a></i>
-
-"MSYS2 is a software distribution consisting of several independent parts, each with their own licenses, comparable to a Linux distribution.
-
-The installer, for example, is based on the qt-installer-framework and pre-packs the direct and indirect dependencies of the base meta package. Each package has its own licenses.
-
-The "pacman" package manager in MSYS2 allows users to install other packages available in our repository, each with their own licenses.
-
-The license information for each package as visible on <a href="https://www.msys2.org/license/">https://packages.msys2.org</a> is maintained on a best effort basis and "we" (quote) make no guarantee that it is accurate or complete."
-
-## vcpkg
-
-vcpkg - C++ Library Manager for Windows, Linux, and MacOS
-
-Copyright (c) Microsoft Corporation
-
-vcpkg is distributed under the MIT License
-
-All rights reserved.
-
-## CMake
-
-CMake - Cross Platform Makefile Generator
-
-Copyright 2000-2023 Kitware, Inc. and Contributors
-
-CMake is distributed under the OSI-approved BSD 3-clause License
-
-All rights reserved.`}
-          />
+            <MDXRemote source={mdx} />
 
             <HRGradient />
 
