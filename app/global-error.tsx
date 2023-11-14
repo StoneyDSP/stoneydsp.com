966ea2ecfdfbef48b8f28de99d336e9e3d48068a
StoneyDSP <nathanjhood@googlemail.com>
2023-11-14 10:22:45 +0100

    Improved error pages
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,4 +1,8 @@
 'use client'
+import 'client-only'
+import { LogoWideL } from '@/components/icons/logo/wide'
+import { useEffect } from 'react'
+import styles from '@/app/layout.module.css'
 
 export default function GlobalError({
   error,
@@ -7,11 +11,40 @@ export default function GlobalError({
   error: Error & { digest?: string }
   reset: () => void
 }) {
+
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
   return (
     <html lang="en">
       <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <main>
+          <div className={styles.container}>
+            <article className={styles.content}>
+
+              <div className={styles.flexboxgrid}>
+
+                <div className="py-4"></div>
+
+                <LogoWideL />
+
+                <h2 className="text-foreground">Something went wrong!</h2>
+                <button
+                  onClick={
+                    // Attempt to recover by trying to re-render the segment
+                    () => reset()
+                  }
+                  className="py-2 px-3 justify-center w-fit rounded-md no-underline transition-colors bg-green-500 hover:bg-purple-400 border-2 hover:border-transparent">
+                  <span className="text-foreground">Try again</span>
+                </button>
+
+              </div>
+
+            </article>
+          </div>
+        </main>
       </body>
     </html>
   )
