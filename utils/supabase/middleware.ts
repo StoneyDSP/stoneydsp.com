028aff512d693a4204a2d6a5ad58856e1c2c51b7
StoneyDSP <nathanjhood@googlemail.com>
2023-11-14 04:32:26 +0100

    CSP
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,79 +1,30 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import generateCsp from '@/utils/headers/generateCSP'
 import { type NextRequest, NextResponse } from 'next/server'
 
-export const createHashedNonce = async (nonce: string) => {
-  const encoder = new TextEncoder()
-  const encodedNonce = encoder.encode(nonce) // Encode the nonce
-  const hash = await crypto.subtle.digest('SHA-256', encodedNonce) // Hash it with SHA-256
-  const bytes = new Uint8Array(hash)
-  const hashedNonce = Array.from(bytes)
-    .map((b) => b.toString(16).padStart(2, '0')) // Convert the hash to a hexadecimal string
-    .join('')
-  return hashedNonce
-}
+
 
 export const createClient = (request: NextRequest) => {
 
-  // Generate a random nonce
-  // Use 'nonce' when invoking the supabase.auth.signInWithIdToken() method
-  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
-  // Use 'hashedNonce' when making the authentication request to Google
-  const hashedNnonce = createHashedNonce(nonce)
-
-  const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-`
-  // const cspHeader = `
-  //   default-src 'self';
-  //   connect-src vitals.vercel-insights.com;
-  //   script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-  //   style-src 'self' 'nonce-${nonce}';
-  //   img-src 'self' blob: data:;
-  //   font-src 'self';
-  //   object-src 'none';
-  //   base-uri 'self';
-  //   form-action 'self';
-  //   frame-ancestors 'none';
-  //   block-all-mixed-content;
-  //   upgrade-insecure-requests;
-  // `
-
-  // Replace newline characters and spaces
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, ' ')
-    .trim()
+  // generate CSP and nonce
+  const { csp, nonce } = generateCsp();
 
   // Clone the request headers
   const requestHeaders = new Headers(request.headers)
+
   // set nonce request header to read in pages if needed
   requestHeaders.set('x-nonce', nonce)
-  requestHeaders.set('X-Content-Type-Options', 'nosniff')
+  requestHeaders.set('x-content-type-options', 'nosniff')
   requestHeaders.set('cache-control', 'public, max-age=0, s-maxage=86400, must-revalidate')
-  requestHeaders.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload')
-  // requestHeaders.set(
-  //   'Content-Security-Policy',
-  //   contentSecurityPolicyHeaderValue
-  // )
-  // requestHeaders.set(
-  //   'Content-Security-Policy-Report-Only',
-  //   contentSecurityPolicyHeaderValue
-  // )
+  requestHeaders.set('strict-transport-security', 'max-age=63072000; includeSubDomains; preload')
+  // Set the CSP header so that `app-render` can read it and generate tags with the nonce
+  requestHeaders.set('content-security-policy', csp);
 
-  // create a new unmodified response
+  // create a new unmodified response...
   let response = NextResponse.next({
     request: {
-      // New request headers
-      headers: request.headers,
+      // ...using the new request headers
+      headers: requestHeaders,
     },
   })
 
@@ -93,7 +44,6 @@ export const createClient = (request: NextRequest) => {
             ...options,
           })
           response = NextResponse.next({
-            headers: request.headers,
             request: {
               headers: requestHeaders,
             },
@@ -112,7 +62,6 @@ export const createClient = (request: NextRequest) => {
             ...options,
           })
           response = NextResponse.next({
-            headers: request.headers,
             request: {
               headers: requestHeaders,
             },
@@ -132,6 +81,13 @@ export const createClient = (request: NextRequest) => {
   //   'Content-Security-Policy',
   //   contentSecurityPolicyHeaderValue
   // )
+
+  // Also set the CSP so that it is outputted to the browser
+  response.headers.set('Content-Security-Policy', csp);
+
+  response.headers.set('x-content-type-options', 'nosniff')
+  response.headers.set('cache-control', 'public, max-age=0, s-maxage=86400, must-revalidate')
+  response.headers.set('strict-transport-security', 'max-age=63072000; includeSubDomains; preload')
 
   return { supabase, response }
 }
