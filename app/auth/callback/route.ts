c354cb0425bd51c36839256d4265d15745d9686e
StoneyDSP <nathanjhood@googlemail.com>
2023-11-15 08:09:50 +0100

    hmmm
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,50 +2,50 @@ import { createClient } from '@/utils/supabase/server'
 import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
-// export async function GET(request: Request) {
-//   // The `/auth/callback` route is required for the server-side auth flow implemented
-//   // by the Auth Helpers package. It exchanges an auth code for the user's session.
-//   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
+export async function GET(request: Request) {
+  // The `/auth/callback` route is required for the server-side auth flow implemented
+  // by the Auth Helpers package. It exchanges an auth code for the user's session.
+  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
 
-//   const { searchParams, origin } = new URL(request.url)
-//   const code = searchParams.get('code')
-//   // if "next" is in param, use it as the redirect URL
-//   const next = searchParams.get('next') ?? '/'
+  const { searchParams, origin } = new URL(request.url)
+  const code = searchParams.get('code')
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get('next') ?? '/'
 
-//   if (code) {
-//     const cookieStore = cookies()
-//     const supabase = createClient(cookieStore)
-//     // await supabase.auth.exchangeCodeForSession(code)
+  if (code) {
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+    // await supabase.auth.exchangeCodeForSession(code)
 
-//     const { error } = await supabase.auth.exchangeCodeForSession(code)
-//     if (!error) {
-//       return NextResponse.redirect(`${origin}${next}`)
-//     }
-//   }
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`)
+    }
+  }
 
-//   // return the user to an error page with instructions
-//   return NextResponse.redirect('/login?message=Under construction...')
+  // return the user to an error page with instructions
+  return NextResponse.redirect('/login?message=Under construction...')
 
-// }
+}
 
 // import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 // import { cookies } from 'next/headers'
 // import { NextResponse } from 'next/server'
 
-import type { NextRequest } from 'next/server'
-// import type { Database } from '@/lib/database.types'
+// import type { NextRequest } from 'next/server'
+// // import type { Database } from '@/lib/database.types'
 
-export async function GET(request: NextRequest) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+// export async function GET(request: NextRequest) {
+//   const requestUrl = new URL(request.url)
+//   const code = requestUrl.searchParams.get('code')
 
-  if (code) {
-    const cookieStore = cookies()
-    // const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore })
-    const supabase = createClient(cookieStore)
-    await supabase.auth.exchangeCodeForSession(code)
-  }
+//   if (code) {
+//     const cookieStore = cookies()
+//     // const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore })
+//     const supabase = createClient(cookieStore)
+//     await supabase.auth.exchangeCodeForSession(code)
+//   }
 
-  // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin)
-}
+//   // URL to redirect to after sign in process completes
+//   return NextResponse.redirect(requestUrl.origin)
+// }
