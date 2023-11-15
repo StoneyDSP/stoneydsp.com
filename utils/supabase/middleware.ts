c414a2e9b1750cb9ea3603587e639570d57117ff
StoneyDSP <nathanjhood@googlemail.com>
2023-11-15 09:26:19 +0100

    Switched to pkce flow
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -13,6 +13,11 @@ export const createClient = (request: NextRequest) => {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      auth: {
+        detectSessionInUrl: true,
+        flowType: 'pkce',
+        //storage: customStorageAdapter,
+      },
       cookies: {
         get(name: string) {
           return request.cookies.get(name)?.value
@@ -56,6 +61,22 @@ export const createClient = (request: NextRequest) => {
       },
     }
   )
+
+  supabase.auth.onAuthStateChange((event, session) => {
+    if (session !== null) {
+      if (event === 'SIGNED_OUT') {
+        // || event === 'USER_DELETED'
+        // delete cookies on sign out
+        const expires = new Date(0).toUTCString()
+        document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`
+        document.cookie = `my-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`
+      } else if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
+        const maxAge = 100 * 365 * 24 * 60 * 60 // 100 years, never expires
+        document.cookie = `my-access-token=${session.access_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`
+        document.cookie = `my-refresh-token=${session.refresh_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`
+      }
+    }
+  })
 
   return { supabase, response }
 }
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -4,10 +4,16 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
-  return createServerClient(
+
+  const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      auth: {
+        detectSessionInUrl: true,
+        flowType: 'pkce',
+        //storage: customStorageAdapter,
+      },
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value
@@ -33,4 +39,22 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
       },
     }
   )
+
+  supabase.auth.onAuthStateChange((event, session) => {
+    if (session !== null) {
+      if (event === 'SIGNED_OUT') {
+        // || event === 'USER_DELETED'
+        // delete cookies on sign out
+        const expires = new Date(0).toUTCString()
+        document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`
+        document.cookie = `my-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`
+      } else if (event === 'SIGNED_IN' || event === 'TOKEN_REFRESHED') {
+        const maxAge = 100 * 365 * 24 * 60 * 60 // 100 years, never expires
+        document.cookie = `my-access-token=${session.access_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`
+        document.cookie = `my-refresh-token=${session.refresh_token}; path=/; max-age=${maxAge}; SameSite=Lax; secure`
+      }
+    }
+  })
+
+  return supabase
 }
