6965a915428e2d037fe40a7133cb3e99c216c4e4
Nathan <nathanjhood@googlemail.com>
2023-11-18 04:46:24 +0100

    Update header.tsx
    Signed-off-by: Nathan <nathanjhood@googlemail.com>

--- components/elements/header/header.tsx
+++ components/elements/header/header.tsx
@@ -1,15 +1,7 @@
-import { usePathname } from 'next/navigation'
 import { Nav } from '@/components/elements'
 import NavBreadcrumbs from '@/components/elements/nav/breadcrumbs'
-// import { Suspense } from 'react'
 
 export default async function Header() {
-
-  const pathname = usePathname()
-
-  const addHome = 'home' + pathname
-
-  const spacedSlashes = addHome.replaceAll(`/`, ` / &`).split(`&`)
 
   return (
     <header>
