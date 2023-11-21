4fa445f35a6ffef7c1763f3e6b878ee17706d25b
StoneyDSP <nathanjhood@googlemail.com>
2023-11-21 20:24:28 +0100

    Checking another build warning...

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,4 +1,4 @@
-import { createSupabaseServerSideClient } from '@/utils/supabase/ssr'
+import createSupabaseServerSideClient from '@/utils/supabase/ssr/server'
 import { cookies } from 'next/headers'
 import AccountForm from '@/components/layouts/Account'
 import { redirect } from 'next/navigation'
