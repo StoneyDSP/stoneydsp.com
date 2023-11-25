75a15ddd81aa47ecf808fc960cfa89ca97a6e08c
StoneyDSP <nathanjhood@googlemail.com>
2023-11-25 12:40:07 +0100

    Added new spinner component
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ components/spinner/spinner.tsx
@@ -0,0 +1,20 @@
+import styles from "./spinner.module.css";
+
+export default function LoadingSpinner() {
+  return (
+    <div className={styles.spinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
