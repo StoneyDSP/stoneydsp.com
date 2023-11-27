285d873918ed78c6db58d3c085a9d324df4fa35b
StoneyDSP <nathanjhood@googlemail.com>
2023-11-27 11:05:49 +0100

    optimized middleware
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- middleware.ts
+++ middleware.ts
@@ -12,15 +12,6 @@ export async function middleware(request: NextRequest) {
   // Refresh session if expired - required for Server Components
   const { data: { session }, error } = await supabase.auth.getSession()
 
-  if (error) {
-    console.log(`Supabase Middleware Client returned AuthError: ${error}`)
-    // return errored requests to the www dir
-    return NextResponse.redirect(
-      new URL(`/www${path === "/" ? "" : path}`, url),
-      response
-    )
-  }
-
   if (!session) {
     // return non-users to the www dir
     return NextResponse.rewrite(
@@ -29,33 +20,42 @@ export async function middleware(request: NextRequest) {
     )
   }
 
-  // App router
-  switch (hostname) {
+  if (!error) {
+    // App router
+    switch (hostname) {
 
-  case `www.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
-    // rewrites for www pages
-    return NextResponse.rewrite(new URL(`/www${path === "/" ? "" : path}`, url), response)
+      case `www.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
+        // rewrites for www pages
+        return NextResponse.rewrite(new URL(`/www${path === "/" ? "" : path}`, url), response)
 
-  case `api.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
-    // rewrites for api pages
-    return NextResponse.rewrite(new URL(`/api${path === "/" ? "" : path}`, url), response)
+      case `api.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
+        // rewrites for api pages
+        return NextResponse.rewrite(new URL(`/api${path === "/" ? "" : path}`, url), response)
 
-  case `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
-    // rewrites for app pages
-    return NextResponse.rewrite(new URL(`/app${path === "/" ? "" : path}`, url), response)
+      case `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
+        // rewrites for app pages
+        return NextResponse.rewrite(new URL(`/app${path === "/" ? "" : path}`, url), response)
 
-  case `blog.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
-    // rewrites for blog pages
-    return NextResponse.rewrite(new URL(`/blog${path === "/" ? "" : path}`, url), response)
+      case `blog.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
+        // rewrites for blog pages
+        return NextResponse.rewrite(new URL(`/blog${path === "/" ? "" : path}`, url), response)
 
-  case `docs.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
-    // rewrites for docs pages
-    return NextResponse.rewrite(new URL(`/docs${path === "/" ? "" : path}`, url), response)
+      case `docs.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`:
+        // rewrites for docs pages
+        return NextResponse.rewrite(new URL(`/docs${path === "/" ? "" : path}`, url), response)
 
-  default:
-    // rewrite everything else to the www dir
-    return NextResponse.rewrite(new URL(`/www${path === "/" ? "" : path}`, url), response)
+      default:
+        // rewrite everything else to the www dir
+        return NextResponse.rewrite(new URL(`/www${path === "/" ? "" : path}`, url), response)
+      }
   }
+
+  console.log(`Supabase Middleware Client returned AuthError: ${error}`)
+  // return errored requests to the www dir
+  return NextResponse.redirect(
+    new URL(`/www${path === "/" ? "" : path}`, url),
+    response
+  )
 }
 
 export const config = {
