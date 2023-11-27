69c8ae96d57226138a813ac13bbcb6a610367a8b
StoneyDSP <nathanjhood@googlemail.com>
2023-11-27 19:26:04 +0100

    More icons for the manifest build
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- app/manifest.tsx
+++ app/manifest.tsx
@@ -11,6 +11,15 @@ import AndroidIcon72x72 from '@/public/images/favicon/android-icon-72x72.png'
 import AndroidIcon96x96 from '@/public/images/favicon/android-icon-96x96.png'
 import AndroidIcon144x144 from '@/public/images/favicon/android-icon-144x144.png'
 
+import Favicon32x32 from '@/public/images/favicon/favicon__32x32.ico'
+import Favicon48x48 from '@/public/images/favicon/favicon__48x48.ico'
+import Favicon64x64 from '@/public/images/favicon/favicon__64x64.ico'
+import Favicon96x96 from '@/public/images/favicon/favicon__96x96.ico'
+
+import Favicon144x144 from '@/public/images/favicon/favicon__144x144.ico'
+import Favicon168x168 from '@/public/images/favicon/favicon__168x168.ico'
+import Favicon256x256 from '@/public/images/favicon/favicon__256x256.ico'
+
 import WIcon196x128 from '@/public/images/w_icon__196x128.png'
 import WIcon384x256 from '@/public/images/w_icon__384x256.png'
 import WIcon768x512 from '@/public/images/w_icon__768x512.png'
@@ -83,60 +92,60 @@ export default async function webmanifest(): Promise<Manifest> {
         type: 'image/png'
       },
       {
-        src: `/images/favicon/favicon__32x32.ico`,
+        src: Favicon32x32.src,
         sizes: '32x32',
         type: 'image/x-icon'
       },
       {
-        src: `/images/favicon/favicon__48x48.ico`,
+        src: Favicon48x48.src,
         sizes: '48x48',
         type: 'image/x-icon'
       },
       {
-        src: `/images/favicon/favicon__64x64.ico`,
+        src: Favicon64x64.src,
         sizes: '64x64',
         type: 'image/x-icon'
       },
       {
-        src: `/images/favicon/favicon__96x96.ico`,
+        src: Favicon96x96.src,
         sizes: '96x96',
         type: 'image/x-icon'
       },
       {
-        src: `/images/favicon/favicon__144x144.ico`,
+        src: Favicon144x144.src,
         sizes: '144x144',
         type: 'image/x-icon'
       },
       {
-        src: `/images/favicon/favicon__168x168.ico`,
+        src: Favicon168x168.src,
         sizes: '168x168',
         type: 'image/x-icon'
       },
       {
-        src: `/images/favicon/favicon__256x256.ico`,
+        src: Favicon256x256.src,
         sizes: '256x256',
         type: 'image/x-icon'
       },
-      {
-        src: `/images/favicon/favicon-32x32.png`,
-        sizes: '32x32',
-        type: 'image/png'
-      },
-      {
-        src: `/images/favicon/favicon-64x64.png`,
-        sizes: '64x64',
-        type: 'image/png'
-      },
-      {
-        src: `/images/favicon/favicon-168x168.png`,
-        sizes: '168x168',
-        type: 'image/png'
-      },
-      {
-        src: `/images/favicon/favicon-256x256.png`,
-        sizes: '256x256',
-        type: 'image/png'
-      },
+      // {
+      //   src: `/images/favicon/favicon-32x32.png`,
+      //   sizes: '32x32',
+      //   type: 'image/png'
+      // },
+      // {
+      //   src: `/images/favicon/favicon-64x64.png`,
+      //   sizes: '64x64',
+      //   type: 'image/png'
+      // },
+      // {
+      //   src: `/images/favicon/favicon-168x168.png`,
+      //   sizes: '168x168',
+      //   type: 'image/png'
+      // },
+      // {
+      //   src: `/images/favicon/favicon-256x256.png`,
+      //   sizes: '256x256',
+      //   type: 'image/png'
+      // },
       {
         src: `/images/favicon/ms-icon-70x70.png`,
         sizes: '70x70',
