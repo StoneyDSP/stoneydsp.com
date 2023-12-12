fcac19e64bba55283e4f10feee8090a6aa8a60a1
StoneyDSP <nathanjhood@googlemail.com>
2023-12-12 13:45:48 +0100

    fixed
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const getSiteURL = () => {
   let url
   switch (process?.env?.VERCEL_ENV) {
     case 'production': {
-      url = process?.env?.NEXT_PUBLIC_SITE_URL
+      url = process?.env?.NEXT_PUBLIC_ROOT_DOMAIN
     }
     case 'preview': {
       url = process?.env?.VERCEL_URL
